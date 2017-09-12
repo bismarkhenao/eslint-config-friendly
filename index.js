993b2109ea59f3cc6586d0dda42fd51921639d4f
Bismark Henao <bismarkhenao7@gmail.com>
2017-09-12 23:19:50 +0200

    ECMAScript 6 Rules
    The code to be parsed could have features from es6,
    NodeJS last version has support for almost all the lastet
    features of ES6, also encourage the new developers to work
    with ES6 with a consistent way.
    
    * Add ECMAScript 6 Rules

--- index.js
+++ index.js
@@ -129,6 +129,48 @@ module.exports = {
     "switch-colon-spacing": 2,
     "template-tag-spacing": 2,
     "unicode-bom": 2,
-    "wrap-regex": 2
+    "wrap-regex": 2,
+    // ECMAScript 6
+    "arrow-body-style": [ 2, "as-needed" ],
+    "arrow-parens": [ 2, "as-needed", { "requireForBlockBody": true }],
+    "arrow-spacing": 2,
+    "constructor-super": 2,
+    "generator-star-spacing": [ 2, {
+      "before": false,
+      "after": true
+    }],
+    "no-class-assign": 2,
+    "no-confusing-arrow": [ 2, { "allowParens": true }],
+    "no-const-assign": 2,
+    "no-dupe-class-members": 2,
+    "no-duplicate-imports": [ 2, { "includeExports": true }],
+    "no-new-symbol": 2,
+    "no-restricted-imports": 0,
+    "no-this-before-super": 2,
+    "no-useless-computed-key": 2,
+    "no-useless-constructor": 2,
+    "no-useless-rename": 2,
+    "no-var": 2,
+    "object-shorthand": [ 2, "always", {
+      "avoidQuotes": true,
+      "ignoreConstructors": true,
+      "avoidExplicitReturnArrows": true
+    }],
+    "prefer-arrow-callback": 2,
+    "prefer-const": [ 2, {
+      "destructuring": "all",
+      "ignoreReadBeforeAssign": true
+    }],
+    "prefer-destructuring": 2,
+    "prefer-numeric-literals": 2,
+    "prefer-rest-params": 2,
+    "prefer-spread": 2,
+    "prefer-template": 2,
+    "require-yield": 2,
+    "rest-spread-spacing": [ 2, "never" ],
+    "sort-imports": 2,
+    "symbol-description": 2,
+    "template-curly-spacing": 2,
+    "yield-star-spacing": 2
   }
 };
